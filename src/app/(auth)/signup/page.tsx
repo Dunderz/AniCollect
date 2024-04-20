c6e092101ced7db3b72da56b08abe3f6ad05a7e7
Dunderz <isaacknam711@gmail.com>
2024-04-19 23:56:24 -0400

    signup redirect

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -106,7 +106,11 @@ export default function Signup() {
                   !passwordAgain ||
                   password !== passwordAgain
                 }
-                onClick={async () => await signup()}
+                onClick={() =>
+                  signup().then(() => {
+                    router.push("/");
+                  })
+                }
                 className="disabled:opacity-40 flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Sign Up
